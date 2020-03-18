71f0866491a3fabd1f04a42bb24e11d0e1dda69d
Wozniaxos <kacperwozniax@gmail.com>
2020-03-18 16:17:14 +0100

    Create post and user view

---
+++ backend/lib/til_web/views/post_view.ex
@@ -0,0 +1,28 @@
+defmodule TilWeb.PostView do
+  use TilWeb, :view
+  alias Til.Repo
+
+  def render("create.json", %{post: post}) do
+    post
+    |> serialize_post
+  end
+
+  def render("index.json", %{posts: posts}) do
+    posts
+    |> Enum.map(&serialize_post/1)
+  end
+
+  defp serialize_post(post) do
+    %{
+      data: %{
+        id: post.id,
+        title: post.title,
+        body: post.body,
+        isPublic: post.is_public,
+        likesCount: post.likes_count,
+        author: render(Til.UserView, "show.json", user: post.author),
+        categoriesIds: Enum.map(post.categories, & &1.id)
+      }
+    }
+  end
+end
