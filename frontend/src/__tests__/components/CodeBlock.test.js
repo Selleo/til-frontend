d13c335f5e576ae4945c44ffbf6331c195bdc2d3
Marek Kazimierczak <marek@Mareks-MacBook-Pro.local>
2020-04-14 18:34:24 +0200

    CodeBlock component test

---
+++ frontend/src/__tests__/components/CodeBlock.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import CodeBlock from "../../components/CodeBlock";
+
+describe("<CodeBlock/>", () => {
+  it("renders properly", () => {
+    const { asFragment } = render(<CodeBlock />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
