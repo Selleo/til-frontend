f7b2668a015e64af2e98fa472f5f97e4362ba229
Marek Kazimierczak <marek@Mareks-MacBook-Pro.local>
2020-04-27 21:32:56 +0200

    Setup ESLint, Prettier & Airbnb Style Guide

--- frontend/src/components/UserPosts.js
+++ frontend/src/components/UserPosts.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
-import { fetchUserPosts } from "../utils";
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { fetchUserPosts } from '../utils';
 
-const UserPosts = props => {
+const UserPosts = () => {
   const [userPosts, setUserPosts] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
     const posts = async () => {
-      const posts = await fetchUserPosts("/api/users/", id);
+      const posts = await fetchUserPosts('/api/users/', id);
 
       setUserPosts(posts);
     };
@@ -19,7 +19,7 @@ const UserPosts = props => {
   return (
     <>
       <h3>Users posts</h3>
-      {userPosts.map(post => (
+      {userPosts.map((post) => (
         <Link to={`/posts/${post.id}`} key={post.id}>
           <p>{post.title}</p>
         </Link>
