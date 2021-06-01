6e02df6b83e68d1b5ec2b98e126d46c92d983bc8
Lukasz  Walczak <lukeabove@gmail.com>
2021-06-01 11:47:28 +0200

    disable input on some routes

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { saveSearchedPosts, saveSearchedQuery } from '../store/actions/actions'
 import { useOnRouteLeave } from '../utils/customHooks/useOnRouteLeave'
 import { ReactComponent as SearchIcon } from '../assets/icons/search.svg'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Search = () => {
   const [input, setInput] = useState('')
   const [timeoutID, setTimeoutID] = useState(null)
+  const [disableInput, setDisableInput] = useState(false)
   const dispatch = useDispatch()
   const history = useHistory()
+  const location = useLocation()
   const hasLeavedRoute = useOnRouteLeave('/search')
 
   useEffect(() => {
@@ -18,6 +22,17 @@ const Search = () => {
       setInput('')
     }
   }, [hasLeavedRoute, dispatch])
+
+  useEffect(() => {
+    if (
+      location.pathname.includes('add') ||
+      location.pathname.includes('edit')
+    ) {
+      setDisableInput(true)
+    } else {
+      setDisableInput(false)
+    }
+  }, [location])
 
   const handleInput = event => {
     const targetValue = event.target.value
@@ -35,16 +50,19 @@ const Search = () => {
       setTimeoutID(timeout)
     }
   }
+  const notify = () => toast("Can't search while post is creating/editing")
 
   return (
-    <div className="search-box">
+    <div className="search-box" onClick={disableInput && notify}>
       <input
         className="search-box__input"
         type="text"
         placeholder="Search"
         value={input}
+        disabled={disableInput}
         onChange={handleInput}
       />
+
       <SearchIcon className="search-box__icon" />
     </div>
   )
