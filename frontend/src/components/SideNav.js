05ec846b93191754e0f85a9b991b5366ee347ebf
lukaszbielsko <lukeabove@gmail.com>
2021-06-15 14:40:07 +0200

    feat: block click on logo while create/edit post

--- frontend/src/components/SideNav.js
+++ frontend/src/components/SideNav.js
@@ -2,25 +2,38 @@ import React from 'react'
 import Categories from '../components/Categories'
 import Search from './Search'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { useIsOnRoute } from '../utils/customHooks/useIsOnRoute'
 
-const SideNav = () => (
-  <div className="side-nav">
-    <div className="logo">
-      <Link to="/" className="logo__link">
-        todayilearned
-      </Link>
+const SideNav = () => {
+  const disable = useIsOnRoute(['add', 'edit'])
+
+  const handleClick = e => {
+    if (disable) {
+      e.preventDefault()
+      toast("Can't go to home while post is creating/editing")
+    }
+  }
+
+  return (
+    <div className="side-nav">
+      <div className="logo">
+        <Link to="/" className="logo__link" onClick={handleClick}>
+          todayilearned
+        </Link>
+      </div>
+      <ul className="side-nav__menu">
+        <li className="side-nav__menu-item">
+          <Search />
+        </li>
+        {/* <li className="side-nav__menu-item">Users</li> */}
+        {/* <li className="side-nav__menu-item">Stats </li> */}
+      </ul>
+      <div className="categories">
+        <Categories />
+      </div>
     </div>
-    <ul className="side-nav__menu">
-      <li className="side-nav__menu-item">
-        <Search />
-      </li>
-      {/* <li className="side-nav__menu-item">Users</li> */}
-      {/* <li className="side-nav__menu-item">Stats </li> */}
-    </ul>
-    <div className="categories">
-      <Categories />
-    </div>
-  </div>
-)
+  )
+}
 
 export default SideNav
