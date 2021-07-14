3576e7cfb66bc217c1fce06681590b0136bdde58
matasin <matasinqwer@gmail.com>
2021-07-14 14:47:59 +0200

    fix: empty post list on single category page when  refresh

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Post from './Post'
 import '../App.css'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { saveCategoryPosts } from '../store/actions/actions'
 
 const PostsList = props => {
   const { posts } = props
+  const categories = useSelector(state => state.categories)
+  const { id } = useParams()
+  const dispatch = useDispatch()
 
   if (!posts) {
+    const foundCategory = categories.find(({ name }) => name === id)
+    if (foundCategory) {
+      dispatch(saveCategoryPosts(foundCategory.id))
+    }
+
     return null
   }
 
