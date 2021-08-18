f460cf1af205f6f2c0d964df4a1a33d816ca339a
matasin <matasinqwer@gmail.com>
2021-08-18 14:40:25 +0200

    feat: elastic transition component

--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -3,17 +3,20 @@ import AdminPanel from '../authenticated/AdminPanel'
 import useUser from '../utils/customHooks/useUser'
 import { Link } from 'react-router-dom'
 import LogInButton from './LogInButton'
+import { Transition } from './Transition'
 
 const AppHeader = () => {
   const user = useUser()
 
   return (
-    <div className="app-header">
-      <Link to="/" className="logo__link">
-        todayilearned
-      </Link>
-      {user ? <AdminPanel /> : <LogInButton className="login-link" />}
-    </div>
+    <Transition name="opacity-animation">
+      <div className="app-header">
+        <Link to="/" className="logo__link">
+          todayilearned
+        </Link>
+        {user ? <AdminPanel /> : <LogInButton className="login-link" />}
+      </div>
+    </Transition>
   )
 }
 
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -7,9 +7,10 @@ import ReactionBar from './ReactionBar'
 import { Link, useLocation } from 'react-router-dom'
 import TextBlock from './TextBlock'
 import { format, parseISO } from 'date-fns'
+import { Transition } from './Transition'
 
 const Post = props => {
-  const { post, userPost, userImage, review } = props
+  const { post, userPost, userImage, review, animationDelay } = props
   const location = useLocation()
 
   let title
@@ -23,37 +24,42 @@ const Post = props => {
   const date = format(parsed, ' dd MMM  hh:mm')
 
   return (
-    <article className="post">
-      <div className="post__header">
-        <div className="post__details">
-          <img
-            src={post.author.image || userImage}
-            className="user__image"
-            alt="author-img"
-          />
-          <div className="post__text-details">
-            <div>
-              {post.author.firstName} {post.author.lastName}
+    <Transition name="post-animation" delay={animationDelay}>
+      <article
+        className="post"
+        style={{ transitionDelay: `${animationDelay}ms` }}
+      >
+        <div className="post__header">
+          <div className="post__details">
+            <img
+              src={post.author.image || userImage}
+              className="user__image"
+              alt="author-img"
+            />
+            <div className="post__text-details">
+              <div>
+                {post.author.firstName} {post.author.lastName}
+              </div>
+              <div className="post__date">{date}</div>
             </div>
-            <div className="post__date">{date}</div>
           </div>
+          {!review && <CopyPostURL postId={post.id} />}
         </div>
-        {!review && <CopyPostURL postId={post.id} />}
-      </div>
-      <div>
-        <Link className="post__title" to={`/posts/${post.id}`}>
-          {title}
-        </Link>
-      </div>
-      <div className="post__body">
-        <Markdown source={post.body} />
-      </div>
-      <div className="post__footer">
-        <PostCategories categories={post.categories} />
-        {!review && <ReactionBar post={post} />}
-      </div>
-      {userPost && <UserPostMenu post={post} />}
-    </article>
+        <div>
+          <Link className="post__title" to={`/posts/${post.id}`}>
+            {title}
+          </Link>
+        </div>
+        <div className="post__body">
+          <Markdown source={post.body} />
+        </div>
+        <div className="post__footer">
+          <PostCategories categories={post.categories} />
+          {!review && <ReactionBar post={post} />}
+        </div>
+        {userPost && <UserPostMenu post={post} />}
+      </article>
+    </Transition>
   )
 }
 
