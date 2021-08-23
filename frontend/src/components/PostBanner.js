a518f4958fe9896fe6ac45212aa9b6fa0cdb38a7
matasin <matasinqwer@gmail.com>
2021-08-23 14:36:22 +0200

    fix: error handling on postBanner

--- frontend/src/components/PostBanner.js
+++ frontend/src/components/PostBanner.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import LogoSelleo from './LogoSelleo'
+import { Transition } from './Transition'
 
 const PostBanner = props => {
   const { postCategory } = props
+
+  if (!postCategory) {
+    return null
+  }
+
   const { url, firstText, secondText } = postCategory
 
   if (!url) {
@@ -10,31 +16,33 @@ const PostBanner = props => {
   }
 
   return (
-    <div className="post-banner">
-      <header className="post-banner__header">
-        <img
-          className="post-banner__bg"
-          src={`${process.env.PUBLIC_URL}/assets/images/banner-bg.svg`}
-          alt="banner"
-        />
+    <Transition name="opacity-animation">
+      <div className="post-banner">
+        <header className="post-banner__header">
+          <img
+            className="post-banner__bg"
+            src={`${process.env.PUBLIC_URL}/assets/images/banner-bg.svg`}
+            alt="banner"
+          />
 
-        <LogoSelleo className="post-banner__logo" />
+          <LogoSelleo className="post-banner__logo" />
 
-        <div className="post-banner__headers">
-          <h3>{firstText}</h3>
-          <h3>{secondText}</h3>
-        </div>
+          <div className="post-banner__headers">
+            <h3>{firstText}</h3>
+            <h3>{secondText}</h3>
+          </div>
 
-        <a
-          href={url}
-          rel="noopener noreferrer"
-          target="_blank"
-          className="post-banner__learn-more"
-        >
-          learn more
-        </a>
-      </header>
-    </div>
+          <a
+            href={url}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="post-banner__learn-more"
+          >
+            learn more
+          </a>
+        </header>
+      </div>
+    </Transition>
   )
 }
 
