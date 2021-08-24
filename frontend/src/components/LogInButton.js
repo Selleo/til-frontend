fe10c3fc892c36db66cf695fbba9bc62c6e69dcf
matasin <matasinqwer@gmail.com>
2021-08-24 14:37:39 +0200

    feat: log in first for review post

--- frontend/src/components/LogInButton.js
+++ frontend/src/components/LogInButton.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 const LogInButton = props => {
   const { REACT_APP_API_URL: API_URL } = process.env
-  const { className } = props
+  const { className, text, handleClick } = props
+
+  const linkText = text ? text : 'log in'
 
   return (
-    <a className={className} href={`${API_URL}/auth/google`}>
-      log in
+    <a
+      className={className}
+      onClick={handleClick}
+      href={`${API_URL}/auth/google`}
+    >
+      {linkText}
     </a>
   )
 }
