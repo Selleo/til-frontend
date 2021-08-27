753ff44f4b3c2504c0ae2ff3f125e5571c80d630
Wozniaxos <kacperwozniax@gmail.com>
2021-08-27 11:44:18 +0200

    feat: remove redundant show and nest in post category view

--- backend/lib/til_web/views/post/category_view.ex
+++ backend/lib/til_web/views/post/category_view.ex
@@ -6,28 +6,12 @@ defmodule TilWeb.PostCategoryView do
     |> Enum.map(&serialize_category/1)
   end
 
-  def render("show.json", %{category: category}) do
-    serialize_category(category, :with_posts)
-  end
-
   # private
 
   defp serialize_category(category) do
     %{
       id: category.id,
       name: category.name,
-      position: category.position,
-      url: category.url,
-      firstText: category.first_text,
-      secondText: category.second_text,
-    }
-  end
-
-  defp serialize_category(category, :with_posts) do
-    %{
-      id: category.id,
-      name: category.name,
-      posts: render(TilWeb.PostView, "index_with_nested.json", posts: Enum.map(category.posts_categories, &(&1.post))),
       position: category.position,
       url: category.url,
       firstText: category.first_text,
