69684d3da3343cb4c8fbfdd4c5b25e0b2a39b99d
monika1127 <dziubmonika@gmail.com>
2021-12-01 00:23:56 +0100

    Add 'hashed_id' verification

--- frontend/src/components/SearchedPosts.js
+++ frontend/src/components/SearchedPosts.js
@@ -3,18 +3,12 @@ import { useSelector } from 'react-redux'
 import SearchedPhrase from './SearchedPhrase'
 import PostsList from '../components/PostsList'
 import NothingFound from './NothingFound'
-import { useHistory } from 'react-router-dom'
 
 const SearchedPosts = () => {
   const searchedPosts = useSelector(state => state.searchedPosts)
   const searchQuery = useSelector(state => state.searchQuery)
-  const history = useHistory()
 
-  if (searchQuery.length < 0) {
-    history.push('/')
-  }
-
-  if (!searchedPosts.length) {
+  if (!searchedPosts?.length) {
     return searchQuery && <NothingFound text={searchQuery} />
   }
 
