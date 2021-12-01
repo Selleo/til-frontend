4ab7c38367520254a28ae1d4e2ac47a6329de5bc
Milosz Zajonc <milosz.zajonc@gmail.com>
2021-12-01 12:12:04 +0100

    test: Add test for DisplaPost component (#90)

---
+++ frontend/src/tests/mocks/post.mock.js
@@ -0,0 +1,29 @@
+export const Post = {
+  createdAt: '2021-11-28T22:15:41',
+  id: 1,
+  isPublic: true,
+  reactionCount: 0,
+  reactions: [],
+  reviewed: true,
+  slug: 'post-slug',
+  title: 'Post Title',
+  body: 'Post content',
+  author: {
+    email: 'john.doe@example.com',
+    firstName: null,
+    image: 'https://google.com',
+    lastName: null,
+    userName: 'johndoe',
+    uuid: '12345',
+  },
+  categories: [
+    {
+      firstText: null,
+      id: 1,
+      name: 'category',
+      position: 1,
+      secondText: null,
+      url: null,
+    },
+  ],
+}
