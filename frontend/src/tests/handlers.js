806c814d4ff7a2bb9f4d111ccfa88bb810b06b3e
ZajoncM <52612822+ZajoncM@users.noreply.github.com>
2021-12-01 16:45:23 +0100

    feat: Add slug for post url (#90) (#107)
    * feat: Add slug for post url (#90)
    
    * test: Add test for DisplaPost component (#90)
    
    * nit: Correct code after review (#90)

---
+++ frontend/src/tests/handlers.js
@@ -0,0 +1,11 @@
+import { rest } from 'msw'
+import { Post } from './mocks/post.mock'
+
+export const apiUrl = process.env.REACT_APP_API_URL
+
+export const handlers = [
+  rest.get(`${apiUrl}/api/posts/:id`, (req, res, ctx) => {
+    const { id } = req.params
+    return res(ctx.json({ ...Post, id }))
+  }),
+]
