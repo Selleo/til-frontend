bddb7cd97caeb7424c9bafa65bde63d6f9d8e4ad
Mateusz <m.dobija@selleo.com>
2021-12-20 15:08:02 +0100

    feat: feat: delete Selleo logo from the footer & resize the icons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,30 +1,23 @@
 import React from 'react'
+
 import { socialMedia } from '../utils/sociaMedia'
-import LogoSelleo from './LogoSelleo'
 
 const Footer = () => (
   <div className="footer">
     <ul className="social-links">
-      {socialMedia.map(({ id, name, href, svg }) => {
-        return (
-          <li key={id}>
-            <a
-              href={href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`social-links__link -${name}`}
-            >
-              {svg}
-            </a>
-          </li>
-        )
-      })}
+      {socialMedia.map(({ id, name, href, svg }) => (
+        <li key={id}>
+          <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`social-links__link -${name}`}
+          >
+            {svg}
+          </a>
+        </li>
+      ))}
     </ul>
-    <div className="footer__logo">
-      <div className="logo">
-        <LogoSelleo />
-      </div>
-    </div>
   </div>
 )
 
