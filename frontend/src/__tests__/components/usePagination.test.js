23cee5099bae196219e0121a1f1b1d8f8e283f0c
ZajoncM <milosz.zajonc@gmail.com>
2022-01-05 08:29:18 +0100

    test: add test for pagination hook

---
+++ frontend/src/__tests__/components/usePagination.test.js
@@ -0,0 +1,33 @@
+import { usePagination } from '../../utils/customHooks/usePagination'
+
+describe('usePagination', () => {
+  it('should generate array of pagination elements for first page', () => {
+    const posts = { pageNumber: 1, totalPages: 10 }
+
+    const result = [1, 2, 3, 4, 5, '...', 10, 'Next']
+
+    const pagination = usePagination(posts)
+
+    expect(pagination).toEqual(result)
+  })
+
+  it('should generate array of pagination elements for middle page', () => {
+    const posts = { pageNumber: 4, totalPages: 15 }
+
+    const result = ['Prev', 1, '...', 3, 4, 5, '...', 15, 'Next']
+
+    const pagination = usePagination(posts)
+
+    expect(pagination).toEqual(result)
+  })
+
+  it('should generate array of pagination elements for last page', () => {
+    const posts = { pageNumber: 20, totalPages: 20 }
+
+    const result = ['Prev', 1, '...', 16, 17, 18, 19, 20]
+
+    const pagination = usePagination(posts)
+
+    expect(pagination).toEqual(result)
+  })
+})
