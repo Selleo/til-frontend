bab28e1e307943076a2a26b57612adb6d2c4ce9b
Mateusz <m.dobija@selleo.com>
2022-02-02 11:47:52 +0100

    refactor: simple refactoring

--- frontend/src/components/CopyButton.js
+++ frontend/src/components/CopyButton.js
@@ -1,15 +1,12 @@
 import React from 'react'
+
 import { ReactComponent as Share } from '../assets/icons/share.svg'
 
-const CopyButton = props => {
-  const { isInTooltip, handleClick, text } = props
-
-  return (
-    <button onClick={handleClick} className="post__share-button">
-      {!isInTooltip && <Share />}
-      <span className="post__share-text">{text}</span>
-    </button>
-  )
-}
+const CopyButton = ({ isInTooltip, handleClick, text }) => (
+  <button onClick={handleClick} className="post__share-button">
+    {!isInTooltip && <Share />}
+    <span className="post__share-text">{text}</span>
+  </button>
+)
 
 export default CopyButton
